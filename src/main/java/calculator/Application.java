c5338b4708718cd6a8018db946aafedc2931fdf8
dong2357 <gd091330@gmail.com>
2025-10-17 16:49:04 +0900

    feat: 기본 구분자 구현기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("Input calc string:");
+        String input = Console.readLine();
+
+        int result = add(input);
+
+        System.out.println("result: " + result);
+    }
+
+    public static int add(String text){
+        if (text == null || text.isEmpty()){
+            return 0;
+        }
+
+        String[] numbers = text.split(",|:");
+
+        int sum = 0;
+        for(String numberStr : numbers){
+            sum+= Integer.parseInt(numberStr);
+        }
+
+        return sum;
     }
 }
