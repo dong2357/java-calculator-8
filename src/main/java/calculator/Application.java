c151e3da4e229d678ac5e16a61466400cc05eb89
dong2357 <gd091330@gmail.com>
2025-10-17 18:56:50 +0900

    feat: 커스텀 구분자 기능 추가
    시행착오 내용
    1. 구분자가 연속으로 나올 때 빈 문자열이나 공백문자\r 같은것이 있어서 오류가 난다 생각하여 isEmpty(), trim()을
    추가하였지만 해결되지 않음
    2. 정규식이 여러 줄의 문자열을 제대로 처리하지 못하는가 싶어서 Pattern.DOTALL을 추가하였지만 해결되지 않음
    3. 줄바꿈 코드 값이 \n 코드값 10이 아닌 92와 110이 따로 들어오는 문제를 확인하고 \\n을 \n으로 바꾸고 계산을 하니 해결되었음

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -7,7 +9,9 @@ public class Application {
         System.out.println("Input calc string:");
         String input = Console.readLine();
 
-        int result = add(input);
+        String processedInput = input.replace("\\n", "\n");
+
+        int result = add(processedInput);
 
         System.out.println("result: " + result);
     }
@@ -17,11 +21,27 @@ public class Application {
             return 0;
         }
 
-        String[] numbers = text.split(",|:");
+        String delimiter = ",|:"; // 기본 구분자 설정
+        String numbersText = text;
+
+        // 커스텀 구분자가 무엇인지 확인
+        Matcher m = Pattern.compile("//(.)\n(.*)", Pattern.DOTALL).matcher(text);
+        if(m.find()){ // 커스텀 구분자와 숫자 부분을 분리
+            String customDelimiter = Pattern.quote(m.group(1));
+            numbersText = m.group(2);
+
+            delimiter += "|" + customDelimiter;
+        }
+
+        String[] numbers = numbersText.split(delimiter);
 
         int sum = 0;
         for(String numberStr : numbers){
-            sum+= Integer.parseInt(numberStr);
+            String trimmedStr = numberStr.trim();
+            if (trimmedStr.isEmpty()){
+                continue;
+            }
+            sum+= Integer.parseInt(trimmedStr);
         }
 
         return sum;
